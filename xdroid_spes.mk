68caf1930df9c326335932359edb8e4aae527b91
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-07-04 05:28:03 +0600

    Update and rename aosp_spes.mk to xdroid_spes.mk

--- xdroid_spes.mk
+++ xdroid_spes.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xdroid/config/common_full_phone.mk)
 
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_spes
+PRODUCT_NAME := xdroid_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -26,4 +26,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Target
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+TARGET_SUPPORTS_BLUR := true/false
+
+
